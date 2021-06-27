faeeb388dca1839ceb6b87be278c9409451e9cd7
kamran <khanbrother805@gmail.com>
2021-06-27 17:48:24 +0530

    registration auth-token

--- user_app/views.py
+++ user_app/views.py
@@ -2,17 +2,28 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from .serializers import RegistrationSerializer
 from rest_framework.response import Response
-
+from rest_framework.authtoken.models import Token
+from user_app import models
 # Create your views here.
 
 
-@api_view(['GET','POST'])
+@api_view(['POST',])
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+            data['response'] = 'Registration Successful!!'
+            data['username'] = account.username
+            data['email'] = account.email
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+        else:
+            data = serializer.errors
+        return Response(data)
 
 
 
